120e7ef25956b11f03294157ba68d5cc5dbd4516
kumar-vijay7 <vijay.kumar1@tothenew.com>
2020-03-19 19:51:45 +0530

    EF Assignment

---
+++ EntityFrameworkAssignment/EntityFrameworkDBFApi/Models/entityModels/Customer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EntityFrameworkDBFApi.Models.entityModels
+{
+    public partial class Customer
+    {
+        public Customer()
+        {
+            Orders = new HashSet<Orders>();
+        }
+
+        public int CustomerId { get; set; }
+        public string CustomerName { get; set; }
+        public string CustomerPhone { get; set; }
+        public string CustomerEmail { get; set; }
+        public string CustomerAddresss { get; set; }
+
+        public virtual ICollection<Orders> Orders { get; set; }
+    }
+}
---
+++ EntityFrameworkAssignment/EntityFrameworkDBFApi/Models/entityModels/Orders.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EntityFrameworkDBFApi.Models.entityModels
+{
+    public partial class Orders
+    {
+        public int OrderId { get; set; }
+        public int? ProductId { get; set; }
+        public int? CustomerId { get; set; }
+        public int? Quantity { get; set; }
+
+        public virtual Customer Customer { get; set; }
+        public virtual Products Product { get; set; }
+    }
+}

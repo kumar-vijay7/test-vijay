59ed184184678cdf958eb2f46109f1405c1ba04e
kumar-vijay7 <vijay.kumar1@tothenew.com>
2020-05-07 10:18:17 +0530

    Interacting With Server Work Progress

---
+++ InteractingWithServer/WebServerReact/src/components/FullPost/FullPost.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+
+import './FullPost.css';
+import Axios from 'axios';
+
+class FullPost extends Component {
+    state={
+        loadedPost:null
+    }
+    componentDidUpdate(){
+        if(this.props.id){
+            if(!this.state.loadedPost || (this.state.loadedPost && this.state.loadedPost.id!==this.props.id)){
+                Axios.get('/posts/'+this.props.id)
+                .then(Response=>{
+                    this.setState({loadedPost:Response.data});
+                })
+            }
+           
+        }
+    }
+
+    deletePostHandler=()=>{
+        Axios.delete('/posts/'+this.props.id)
+            .then(response=>{
+                console.log(response);
+                alert("Successfully Deleted Post...")
+            });
+    }
+    render () {
+        let post =   <p style={{textAlign:'center'}}>Please select a Post!</p>;
+        if(this.props.id){
+            post=<p style={{textAlign:'center'}}>Loading....</p>;
+        }
+        if(this.state.loadedPost){
+            post = (
+                <div className="FullPost">
+                    <h1>{this.state.loadedPost.title}</h1>
+                    <p>{this.state.loadedPost.body}</p>
+                    <div className="Edit">
+                        <button onClick={this.deletePostHandler} className="Delete">Delete</button>
+                    </div>
+                </div>
+    
+            );
+        }
+        
+        return post;
+    }
+}
+
+export default FullPost;
